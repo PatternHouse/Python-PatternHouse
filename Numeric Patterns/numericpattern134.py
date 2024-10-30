96ccf4b3cb94044aa4715a0769c04b5fcce6076d
Jenina Angelin <jeninaangelind@gmail.com>
2024-10-30 23:07:14 +0530

    correct logic in numericpattern134.py

--- Numeric Patterns/numericpattern134.py
+++ Numeric Patterns/numericpattern134.py
@@ -1,21 +1,26 @@
-print("Enter the no of rows: ")
-n = int(input())
-count = 1
-for i in range(n):
-    for j in range(n):
-        print(count, end=" ")
-        count+=1
-    if(count>24):
-        count-=10
-    else:
-        count+=5
-    
+print("Enter the number:")
+n=int(input())
+
+for i in range(0,n,2):
+  for j in range(1,n+1):
+    print(n*i+j,end=" ")
+  print()
+
+if(n%2==1):
+  for i in range(n-2,0,-2):
+    for j in range(1,n+1):
+      print(n*i+j,end=" ")
     print()
+else:
+  for i in range(n-1,0,-2):
+    for j in range(1,n+1):
+      print(n*i+j,end=" ")
+    print() 
     
 Enter the no of rows: 
 5
-1 2 3 4 5 
-11 12 13 14 15 
-21 22 23 24 25 
+1 2 3 4 5
+11 12 13 14 15
+21 22 23 24 25
 16 17 18 19 20 
-26 27 28 29 30 
+6 7 8 9 10
